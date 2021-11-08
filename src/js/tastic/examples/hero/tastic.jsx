9a365761bec79a8b05cbc06316ba05eb27d4068d
Andrius <xcintas@gmail.com>
2021-11-08 11:54:53 +0200

    js errors fix

--- src/js/tastic/examples/hero/tastic.jsx
+++ src/js/tastic/examples/hero/tastic.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 import Hero from '../../../patterns/molecules/Hero'
 import tastify from '@frontastic/catwalk/src/js/helper/tastify'
 
-export const HeroTastic = ({ data }) => {
+const HeroTastic = ({ data }) => {
     return <Hero {...data} />
+}
+
+HeroTastic.propTypes = {
+    image: PropTypes.object.isRequired,
+    aspect: PropTypes.string.isRequired,
+    isFullWidth: PropTypes.bool.isRequired,
 }
 
 export default tastify({ translate: true })(HeroTastic)
